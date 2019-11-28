9199d7ce18aceec1acc8b216f13c5efa964f1778
vieir4ndo <matheus.eu.mv@gmail.com>
2019-11-28 13:31:00 -0200

    1

--- model/conexao.php
+++ model/conexao.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$banco = "teste";
+$username = "admin";
+$password = "novaSenha";
+$banco = "maisidoso2";
 
 // Create connection
 $GLOBALS['conn'] = new mysqli($servername, $username, $password, $banco);
